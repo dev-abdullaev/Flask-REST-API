febfff722a11629aff2cec967c701a6fd61261f8
dev-abdullaev <developer.abdullaev@gmail.com>
2023-10-24 20:45:56 +0500

    docs: add docs for all views

--- application/app.py
+++ application/app.py
@@ -1,11 +1,13 @@
+"""
+Main application file
+"""
+
 import os
 from datetime import datetime
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-
 from .services import error_response, success_response, CustomValidationError
-
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
@@ -13,7 +15,6 @@ app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-
 
 # mongodb database
 mongodb_client = PyMongo(app)
@@ -23,14 +24,17 @@ mongo = db
 
 @app.route('/', methods=['GET'])
 def welcome_page():
+    """Welcome page route"""
     return 'Welcome to the application'
+
 
 @app.route('/api/v1/', methods=['GET'])
 def get_all_data():
+    """API endpoint to get all data"""
     objs = mongo.find().sort("_id", -1)
-    if objs is None:
-        raise CustomValidationError(msg='Not found', code=404)
-     
+    if mongo.count_documents({}) == 0:
+        raise CustomValidationError(msg='No objects found', code=404)
+
     new_ls = []
     for obj in objs:
         obj['_id'] = str(obj['_id'])
@@ -40,20 +44,21 @@ def get_all_data():
 
 @app.route('/api/v1/add/', methods=['POST'])
 def add_data():
+    """API endpoint to add data"""
     data = request.json
     key = data.get('key')
     value = data.get('value')
 
     if key is None or value is None:
-        raise CustomValidationError(msg='Not found', code=404)
-    
-    if mongo.count_documents({ 'key': key }, limit = 1) > 0:
+        raise CustomValidationError(msg='Key or value not found', code=400)
+
+    if mongo.count_documents({'key': key}, limit=1) > 0:
         raise CustomValidationError(msg='Object with the same key name already exists.', code=400)
 
     inserted_object = {
-        "key": key, 
-        "value": value, 
-        'created_at': datetime.utcnow(), 
+        "key": key,
+        "value": value,
+        'created_at': datetime.utcnow(),
         'updated_at': datetime.utcnow()
     }
     mongo.insert_one(inserted_object)
@@ -63,6 +68,7 @@ def add_data():
 
 @app.route('/api/v1/update/<string:key>/', methods=['PUT'])
 def update_data(key):
+    """API endpoint to update data"""
     data = request.json
     new_value = data.get('value')
     updated_at = datetime.utcnow()
@@ -72,14 +78,15 @@ def update_data(key):
         result = mongo.find_one({"key": key})
         result['_id'] = str(result['_id'])
         return success_response(results=result), 200
-    return error_response(error='Object not found'), 404
+    return error_response(msg='Object not found'), 404
 
 
 @app.route('/api/v1/get/<string:key>/', methods=['GET'])
 def get_data(key):
-    key = mongo.find_one({"key": key})
-    
-    if key:
-        key['_id'] = str(key['_id'])
-        return success_response(results=key), 200
-    return error_response(), 400
+    """API endpoint to get specific data by key"""
+    obj = mongo.find_one({"key": key})
+
+    if obj:
+        obj['_id'] = str(obj['_id'])
+        return success_response(results=obj), 200
+    return error_response(msg='Object not found'), 404
