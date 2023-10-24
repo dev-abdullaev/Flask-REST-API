c2279baef877cf2d0817d009e10b89bcdadea19f
dev-abdullaev <developer.abdullaev@gmail.com>
2023-10-24 19:35:44 +0500

    fix: Fix the issue with run and pylint files

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+"""
+This module contains the entry point for your application.
+It imports the Flask app object from the application package and runs the application.
+"""
+
 from application.app import app
 
-
 if __name__ == "__main__":
-  app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=8080)
